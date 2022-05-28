4e91790b113d02571cf0b45dc5f4772f0110ec5d
PhilipVigus <philipvigus@yahoo.co.uk>
2022-05-28 17:06:05 +0100

    Initial commit

---
+++ src/main/java/com/philvigus/brewery/BreweryApplication.java
@@ -0,0 +1,13 @@
+package com.philvigus.brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BreweryApplication.class, args);
+  }
+
+}
